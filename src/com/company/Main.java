09323634416ce63808ea23a2a2b0fef5d89f86f6
Muhammed <muhammedtoichubai@gmail.com>
2022-02-07 00:39:55 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,68 @@
+package com.company;
+import java.util.Scanner;
+import java.util.Random;
+public class Main {
+
+    public static void main(String[] args) {
+//    Scanner tapshyrma = new Scanner(System.in);
+//        int konsol = tapshyrma.nextInt();
+//        int ajyratuu1 = konsol/100;
+//        int ajyratuu2 = konsol/10%10;
+//        int ajyratuu3 = konsol%10;
+//        System.out.println(ajyratuu1+","+ajyratuu2+","+ajyratuu3);
+
+//        Scanner ubakyt =new Scanner(System.in);
+//        int konsol1 = ubakyt.nextInt();
+//        int konsol2 = ubakyt.nextInt();
+//        int konsol3 = ubakyt.nextInt();
+//        int minuta = 60; //bir minuta 60 sekundka barabar!
+//        System.out.println((konsol1*minuta*minuta)+(konsol2*minuta)+konsol3);
+
+       /* Консольдон уч сан алып, алардын суммасын, кобойтуусун жана
+        арифметикалык орточо санын табыш керек.
+                Мисалы консольго:
+        5 7 8 деп жазып
+        Жообу катары томондогудой жоопту алышыбыз керек:
+        5 + 7 + 8 = 20
+        5 * 7 * 8 = 280
+        (5 + 7 + 8) / 3 = 6.6666666667 точкадан кийинки сандар да чыгышы керек*/
+//                  i
+
+       /* Scanner num = new Scanner(System.in);
+        float first, second, third, result;
+        first = num.nextFloat();
+        second = num.nextFloat();
+        third = num.nextFloat();
+        result = first+ second+ third;
+        System.out.println(  first +" + " +second+" + "+ third+" = "+ result);
+        result = first* second* third;
+        System.out.println(first +" * " +second+" * "+ third+" = "+ result);
+        result = first+ second+ third;
+        result = result / 3 ;
+        System.out.println( "("+ first +" + " +second+" + "+ third+")"+ " /3 = "+ result);*/
+        
+
+//        Scanner scanner = new Scanner(System.in);
+//        int summa;
+//        for (int i = 1; i <= 512; i++ ) {
+//            summa = i + i;
+//            if (){
+//                System.out.print(summa + " ");
+//            }
+//
+//        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
